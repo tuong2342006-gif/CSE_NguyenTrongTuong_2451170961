695b1b32a787a72c0e5cc2c0a09053221691687f
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-21 01:14:05 +0700

    PBT_07: Hoàn thành bài B2 -  Xử lý dữ liệu sinh viên

--- PBT_07/calculator.js
+++ PBT_07/calculator.js
@@ -37,3 +37,19 @@ function calculate(num1, operator, num2) {
             return `Lỗi: Operator '${operator}' không hợp lệ`;
     }
 }
+// Test
+
+console.log(calculate(10, "+", 5));    
+// → 15
+
+console.log(calculate(10, "/", 0));    
+// → "Lỗi: Không thể chia cho 0"
+
+console.log(calculate(10, "^", 5));    
+// → "Lỗi: Operator '^' không hợp lệ"
+
+console.log(calculate("abc", "+", 5)); 
+// → "Lỗi: Input không phải số"
+
+console.log(calculate(2, "**", 10));   
+// → 1024
